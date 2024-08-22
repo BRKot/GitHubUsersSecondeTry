6029dc0304228e880ce595b77628bbbce7a5420c
BRKot <65824266+BRKot@users.noreply.github.com>
2024-08-22 21:48:32 +0500

    Add files via upload
    Надеюсь боги склоняться предо мной

---
+++ GitHubUsersSecondeTry/GitHubUsersSecondeTry/UsersPresenter.swift
@@ -0,0 +1,129 @@
+import Foundation
+import UIKit
+
+struct User: Codable{
+    let id: Int
+    let login: String
+    var company: String?
+    var name: String?
+    var email: String?
+    var blog: String?
+    var location: String?
+    var public_repos: Int?
+    var folowers: Int?
+    var imageData: Data?
+    var bio: String?
+    var avatar_url: String
+}
+
+struct FullUserInformation: Codable{
+    let id: Int
+    let public_repos: Int
+    let followers: Int
+}
+
+class UsersPresenter{
+    
+    var view: UsersView?
+    var users: [User] = []
+    var selectedUser:((String)->Void)?
+    
+    
+    init(view: UsersView) {
+        self.view = view
+    }
+    
+    func setupUI(){
+        refreshUsers()
+    }
+    
+    var numberCell: Int{
+        return users.count
+    }
+    
+    func refreshUsers(){
+        let apiClient = APIClient<[User]>()
+        let endPoint = UserEndPoint.users
+        
+        apiClient.createRequest(url: "\(endPoint.baseUrl)\(endPoint.endPoint)") { [self] users, error in
+            guard let users = users, error == nil else {
+                self.handleError(error: String(describing: error))
+                return
+            }
+            self.handleSuccesLoadUsers(users: users)
+        }
+    }
+    
+    func loadAddInformation(usersLogin: [String]){
+        let apiClient = APIClient<FullUserInformation>()
+        for user in usersLogin{
+            let endPoint = UserEndPoint.users
+            apiClient.createRequest(url: "\(endPoint.baseUrl)users/\(user)") { addInformation, error in
+                guard let user = addInformation, error == nil else {
+                    self.handleError(error: String(describing: error))
+                    return
+                }
+                self.handleSuccesLoadAddInformation(user: user)
+            }
+        }
+    }
+    
+    func loadAvatars(urls: [String]){
+        let apiClient = APIClient<Int>()
+        for (index, imageUrl) in urls.enumerated(){
+            apiClient.loadImage(url: imageUrl) { data, error in
+                guard let data = data, error == nil else {
+                    return
+                }
+                self.users[index].imageData = data
+                DispatchQueue.main.async{
+                    self.view?.configureAvatars(indexCell: index, userInformation: self.users[index])
+                }
+            }
+        }
+    }
+    
+    func handleError(error: String){
+        
+    }
+    
+    
+    
+    func handleSuccesLoadUsers(users: [User]){
+        self.users = users
+
+        DispatchQueue.main.async {
+            self.view?.refreshCollectionView()
+        }
+        self.loadAddInformation(usersLogin: users.map{$0.login})
+        self.loadAvatars(urls: users.map({$0.avatar_url}))
+    }
+    
+    func handleSuccesLoadAddInformation(user: FullUserInformation){
+        if let index = self.users.firstIndex(where: {$0.id == user.id}){
+            users[index].folowers = user.followers
+            users[index].public_repos = user.public_repos
+            DispatchQueue.main.async{
+                self.view?.configureCell(indexCell: index, userInformation: self.users[index])
+            }
+        }
+    }
+    
+    func configure(cell: UserCell, indexPath: IndexPath){
+        let user = users[indexPath.row]
+        cell.loginLabel.text = user.login
+        if let countFolowers = user.folowers, let countRepos = user.public_repos {
+            cell.countSubscribersLabel.text = "\(countFolowers) подписчиков"
+            cell.countRepositoriesLabel.text = "\(countRepos) репозиториев"
+        }
+        if let imageData = user.imageData{
+            cell.imageView.image = UIImage(data: imageData)
+        }
+        
+    }
+    
+    func selectedCell(index: Int){
+        print(users[index].login)
+        selectedUser!(users[index].login)
+    }
+}
---
+++ GitHubUsersSecondeTry/Pods/Target Support Files/Pods-GitHubUsersSecondeTry/Pods-GitHubUsersSecondeTry-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitHubUsersSecondeTry : NSObject
+@end
+@implementation PodsDummy_Pods_GitHubUsersSecondeTry
+@end
